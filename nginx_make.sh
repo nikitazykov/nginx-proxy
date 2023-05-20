fc3c3ce3f1509e8bbf6e9c71a2aa7b339b689683
Nikita Zykov <nikit@diagit.ru>
2023-05-20 10:27:08 -0300

    Upd of path to conf

--- nginx_make.sh
+++ nginx_make.sh
@@ -5,7 +5,7 @@ PROJECT=$2
 DOMAIN="XXXXX"
 HOST="$Z-$PROJECT.$DOMAIN"
 
-NGINX_CONF=$(cat /server/nginx-proxy/blank.conf!)   > /dev/null
+NGINX_CONF=$(cat /server/nginx-proxy/conf.d/blank.conf!)   > /dev/null
 NGINX_CONF=${NGINX_CONF//XXX.XX/$HOST.ru}
 NGINX_CONF=${NGINX_CONF//CCCC/$DOMAIN}
 echo "${NGINX_CONF//PPP/43$3}" > /server/nginx-proxy/conf.d/$Z-$PROJECT.conf
